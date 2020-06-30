35f83b0a326dae0d27aea3d1e1b84ecf24f848c4
Cristiand187 <cristiand187@hotmail.com>
2020-06-30 23:50:43 +0000

    hBnB - test holberton

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""Modele Test Holberton"""
 from models import storage
 from models.base_model import BaseModel
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
+    obj = all_objs[obj_id].__str__
     print(obj)
 
 print("-- Create a new object --")
