c3a5867ba1590b76ced41475f0e8efa01c6f3dbb
Cristiand187 <cristiand187@hotmail.com>
2020-07-01 03:42:21 +0000

    hBnB - Test pep8

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,3 +2,15 @@
 """unittest for BaseModel"""
 import unittest
 import pep8
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """defining the unittest cases for BaseModel class"""
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["./models/state.py"])
+        self.assertEqual(result.total_errors, 0, "Found code style " +
+                         "errors (and warnings).")
