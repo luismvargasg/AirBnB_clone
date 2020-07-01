088b3e299c7c6b1db5342a363fbfd83d38277db3
luismvargasg <1610@holbertonschool.com>
2020-07-01 03:22:07 +0000

    testing

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,28 +9,28 @@ class FileStorage():
     """class that serializes instances to a JSON file and
     deserializes JSON file to instances
     """
-    __file_path = "file.json"
-    __objects = {}
 
     def __init__(self):
         """constructor to the class FileStorage"""
+        self.__file_path = "file.json"
+        self.__objects = {}
         pass
 
     def all(self):
         """Prints all string representation of all instances
         based or not on the class name.
         """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj.to_dict()
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w") as json_file:
-            json_file.write(json.dumps(FileStorage.__objects))
+        with open(self.__file_path, "w") as json_file:
+            json_file.write(json.dumps(self.__objects))
 
     def reload(self):
         """deserializes the JSON file to __objects ONLY if the JASON file
@@ -38,10 +38,10 @@ class FileStorage():
         should be raised
         """
         try:
-            with open(FileStorage.__file_path, 'r') as json_file:
+            with open(self.__file_path, 'r') as json_file:
                 json_load = json.load(json_file)
             for keys, values in json_load.items():
-                FileStorage.__objects[k] = BaseModel(**values)
+                self.__objects[k] = BaseModel(**values)
         except:
             pass
 
@@ -49,5 +49,5 @@ class FileStorage():
         """deletes an instance based on the class name and id and
         update the JSON file
         """
-        del FileStorage.__objects[key]
-        FileStorage.save()
+        del self.__objects[key]
+        self.save()
