a9b389591770d373399697135876db0a7ba7cf43
luismvargasg <1610@holbertonschool.com>
2020-07-01 02:51:28 +0000

    changinf logic on reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,41 +9,45 @@ class FileStorage():
     """class that serializes instances to a JSON file and
     deserializes JSON file to instances
     """
+    __file_path = "file.json"
+    __objects = {}
 
     def __init__(self):
         """constructor to the class FileStorage"""
-        self.__file_path = "file.json"
-        self.__objects = {}
+        pass
 
     def all(self):
         """Prints all string representation of all instances
         based or not on the class name.
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj.to_dict()
+        FileStorage.__objects[key] = obj.to_dict()
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, "w") as json_file:
-            json_file.write(json.dumps(self.__objects))
+        with open(FileStorage.__file_path, "w") as json_file:
+            json_file.write(json.dumps(FileStorage.__objects))
 
     def reload(self):
-        """deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists; otherwise, nothing happends.
+        """deserializes the JSON file to __objects ONLY if the JASON file
+        exists, otherwise, do nothing.  If the file doesn't exist, exceptions
+        should be raised
         """
         try:
-            with open(self.__file_path, "r") as json_file:
-                self.__objects = json.loads(json_file.read())
-        except Exception:
+            with open(FileStorage.__file_path, 'r') as json_file:
+                json_load = json.load(json_file)
+            for keys, values in json_load.items():
+                FileStorage.__objects[k] = BaseModel(**values)
+        except:
             pass
 
     def delete(self, key):
         """deletes an instance based on the class name and id and
         update the JSON file
         """
-        del self.__objects[key]
-        self.save()
+        del FileStorage.__objects[key]
+        FileStorage.save()
