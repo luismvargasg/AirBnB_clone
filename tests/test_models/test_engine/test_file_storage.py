613271e6ae3f02081dd372f84d2f414d55ade1f9
Cristiand187 <cristiand187@hotmail.com>
2020-07-01 04:13:15 +0000

    hBnB - Test file storege

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,3 +2,15 @@
 """unittest for BaseModel"""
 import unittest
 import pep8
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStore(unittest.TestCase):
+    """defining the unittest cases for FileStore class"""
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["./models/engine/file_storage.py"])
+        self.assertEqual(result.total_errors, 0, "Found code style " +
+                         "errors (and warnings).")
